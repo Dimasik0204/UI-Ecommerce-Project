65653eec9494b2db1726af7963db425328768c36
ElyasO03 <elyas.ozbek07@gmail.com>
2022-10-21 09:53:41 -0400

    update oct21

--- ShowAllDetail.js
+++ ShowAllDetail.js
@@ -11,14 +11,22 @@ async function ShowAllDetailPage(){
     let result = await response.json()
     console.log(result)
 
-    document.getElementById('detailDiv').innerHTML = ` <h2>${result.course}</h2>
+    document.getElementById('detailDiv').innerHTML = ` 
+    <h1 class='bold' >Course: </h1>
+    <h2>${result.course}</h2>
+    <h2 class='bold'>Title:</h2> 
     <h2>${result.title}</h2>
     <img src=${result.photoUrl}/>
-    <h2>${result.cuisine}</h2>
-    <h2>${result.directions}</h2>
-    <h2>${result.mainIngredient}</h2>
-    <h2>${result.calories}</h2>
-    <h2>${result.carbohydrate}</h2>
+    <h3 class='bold'>Cuisine: </h3>
+    <h4>${result.cuisine}</h2>
+    <h3 class='bold'>Directions: </h3>
+    <h4>${result.directions}</h2>
+    <h3 class='bold'>Main Ingredients: </h3>
+    <h4>${result.mainIngredient}</h2>
+    <h3 class='bold'>Calories: </h3>
+    <h4>${result.calories}</h2>
+    <h3 class='bold'>Carbohydrate: </h3>
+    <h4>${result.carbohydrate}</h2>
     `
 }
 
